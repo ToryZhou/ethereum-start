eaf5d2934f66ed643a3446e8be98d9f7a1bf21da
torychow <522617505@qq.com>
2018-05-22 00:05:13 +0800

    Add ethereum config

--- src/main/java/com/job/ethereumstart/controller/EthereumController.java
+++ src/main/java/com/job/ethereumstart/controller/EthereumController.java
@@ -1,10 +1,29 @@
 package com.job.ethereumstart.controller;
 
+import com.job.ethereumstart.ethereum.EthereumBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class EthereumController {
+
+    @Autowired
+    EthereumBean ethereumBean;
+
+    @RequestMapping(value = "/bestBlock", method = GET, produces = APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String getBestBlock() throws IOException {
+        return ethereumBean.getBestBlock();
+    }
+
 
     @GetMapping
     public String get() {
